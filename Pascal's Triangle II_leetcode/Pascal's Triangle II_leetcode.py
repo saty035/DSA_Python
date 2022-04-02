86b1b8f0855e081d9671d8a341d81e1d054e385a
saty035 <71729770+saty035@users.noreply.github.com>
2022-04-03 02:42:57 +0530

    Pascal's Triangle II_leetcode

---
+++ Pascal's Triangle II_leetcode/Pascal's Triangle II_leetcode.py
@@ -0,0 +1,24 @@
+# solved by satyam kumar
+# question link https://leetcode.com/problems/pascals-triangle-ii/
+
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:        
+        lst=[]
+        
+        for i in range(0,rowIndex+1):
+            
+            temp=[]
+            for j in range(0,i+1):
+                
+                # print('i {} j {}'.format(i,j))
+                
+                if  j==0 or i==j:
+                    temp.append(1)
+                else:
+                    temp.append(lst[i-1][j]+lst[i-1][j-1])
+            lst.append(temp)
+        
+        return lst[rowIndex]
+        
+                    
+                
